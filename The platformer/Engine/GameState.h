0fe527f5d7448137facccaa4a5a4a0355bc1a6a0
Nameless <tyrebrown873@gmail.com>
2020-06-13 17:16:04 -0400

    Engine has a getwindow function no need to pass it by ref

--- The platformer/Engine/GameState.h
+++ The platformer/Engine/GameState.h
@@ -7,7 +7,7 @@ class GameState
 public:
     virtual ~GameState();
     virtual void Render() = 0;
-    virtual void Update(sf::RenderWindow& window, const float dt) = 0;
+    virtual void Update(const float dt) = 0;
     virtual void HandleInput() = 0;
 };
 
