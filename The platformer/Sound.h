b80309cd1ceddf6f322cdddd5c55f0d5fbe55e64
Nameless <tyrebrown873@gmail.com>
2020-06-14 11:13:13 -0400

    Added a basic sound class (Need Testing)

---
+++ The platformer/Sound.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <SFML/Audio.hpp>
+
+/*
+	Class used for short Sound files. Ex: gunshots, hitmarkers, jumping, breathing. This isn't used for big music files! Use the music class for that!
+*/
+class Sound
+{
+public:
+	static Sound* GetInstance();
+	~Sound();
+	void Quit();
+public:
+	bool LoadSoundBuffer(const std::string& path);
+	void Play(const std::string& soundID);
+	void Stop(const std::string& soundID);
+	void RemoveSound(const std::string& soundID);
+private:
+	Sound();
+	static Sound* s_instance;
+	std::map<std::string, sf::SoundBuffer> m_soundsbuffers;
+	std::map<std::string, sf::Sound> m_sounds;
+};
+
