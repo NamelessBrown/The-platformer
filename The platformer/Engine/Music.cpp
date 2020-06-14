f668fa46fe1e9a1f16a4a31ee503555263bbd169
Nameless <tyrebrown873@gmail.com>
2020-06-14 13:05:36 -0400

    I had LNK2019 error so I added them in their own file

---
+++ The platformer/Engine/Music.cpp
@@ -0,0 +1,52 @@
+#include "Music.h"
+
+Music* Music::s_instance = nullptr;
+
+Music* Music::GetInstance()
+{
+	if (s_instance == nullptr)
+	{
+		s_instance = new Music();
+	}
+
+	return s_instance;
+}
+
+Music::Music()
+{
+
+}
+
+Music::~Music()
+{
+}
+
+void Music::Quit()
+{
+	delete s_instance;
+}
+
+bool Music::LoadMusic(const std::string& musicID, const std::string& filename)
+{
+	if (!m_music[musicID].openFromFile(filename))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void Music::Play(const std::string& musicID)
+{
+	m_music[musicID].play();
+}
+
+void Music::Stop(const std::string& musicID)
+{
+	m_music[musicID].stop();
+}
+
+void Music::RemoveSound(const std::string& musicID)
+{
+	m_music.erase(musicID);
+}
---
+++ The platformer/Engine/Music.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <SFML/Audio.hpp>
+/* Music class used for big boy files! */
+class Music
+{
+public:
+	static Music* GetInstance();
+	~Music();
+	void Quit();
+public:
+	bool LoadMusic(const std::string& musicID, const std::string& filename);
+	void Play(const std::string& musicID);
+	void Stop(const std::string& musicID);
+	void RemoveSound(const std::string& musicID);
+private:
+	Music();
+	static Music* s_instance;
+	std::map<std::string, sf::Music> m_music;
+};
