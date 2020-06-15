85ca0c0cee4dfdc44cb93912d1af31611f4e42c9
Nameless <tyrebrown873@gmail.com>
2020-06-15 15:27:09 -0400

    lose health now

--- The platformer/PlayState.cpp
+++ The platformer/PlayState.cpp
@@ -11,22 +11,29 @@
 
 PlayState::PlayState()
 {
+	m_level = "level1";
+	m_levelPath = "Levels/level1.tmx";
+
 	m_maps.emplace_back(new Map());
 	Music::GetInstance()->LoadMusic("fath", "Sounds/Faith by Richard Burnish.ogg");
 	Music::GetInstance()->Play("fath");
 	Music::GetInstance()->GetMusic("fath").setLoop(true);
 
 	TextureManager::GetInstance()->LoadFont("font", "Font/times.ttf");
-	MapParser::GetInstance()->Load("level1", "Levels/level1.tmx");
+	MapParser::GetInstance()->Load(m_level, m_levelPath);
 	m_maps[m_currentMap] = MapParser::GetInstance()->GetMap("level1");
 	CollisionHandler::GetInstance()->Init(m_maps[m_currentMap]);
 
 	TextureManager::GetInstance()->LoadTexture("player", "Textures/player.png");
 	TextureManager::GetInstance()->LoadTexture("bomb", "Textures/sheet1.png");
 	TextureManager::GetInstance()->LoadTexture("dead", "Textures/dead.jpg");
-	SpawnBombs(50);
+	TextureManager::GetInstance()->LoadTexture("DDD", "Textures/DDD.png");
+	SpawnBombs(10);
 	m_player = new Player(GameObjectProperties("player", {32, 0}, 46, 50));
 	TextureManager::GetInstance()->GetSprite("bomb").setScale(TextureManager::GetInstance()->GetSprite("bomb").getScale() / 2.f);
+	TextureManager::GetInstance()->GetSprite("DDD").setScale(TextureManager::GetInstance()->GetSprite("DDD").getScale() / 6.f);
+
+	m_goal = new Goal(GameObjectProperties("DDD", { 250, 450 }, 405, 214));
 
 	Camera::GetInstance()->SetTarget(m_player->GetOrigin());
 }
@@ -34,6 +41,7 @@ PlayState::PlayState()
 PlayState::~PlayState()
 {
 	delete m_player;
+	delete m_goal;
 	
 	for (auto& e : m_bombs)
 	{
@@ -52,17 +60,7 @@ void PlayState::Update(const float dt)
 	Camera::GetInstance()->Update(dt);
 	m_player->Update(dt);
 
-	for (unsigned i = 0; i < m_bombs.size(); i++)
-	{
-		m_bombs[i]->Update(dt);
-
-		if (CollisionHandler::GetInstance()->CheckCollision(m_player->GetCollider().GetColliderBox(), m_bombs[i]->GetCollider().GetColliderBox()))
-		{
-			m_bombs[i]->Boom();
-			m_player->SetHealth(m_bombs[i]->GetDamage());
-		}
-
-	}
+	BombCollision(dt);
 
 	if (m_player->GetHealth() < 0)
 	{
@@ -78,14 +76,29 @@ void PlayState::Render()
 
 	m_maps[m_currentMap]->Render();
 	m_player->Render();
+	m_goal->Render();
 
+	for (auto& e: m_bombs)
+	{
+		e->Render();
+	}
+}
+
+void PlayState::BombCollision(const float dt)
+{
 	for (unsigned i = 0; i < m_bombs.size(); i++)
 	{
-		if (m_bombs[i]->IsAlive())
+		m_bombs[i]->Update(dt);
+
+		if (CollisionHandler::GetInstance()->CheckCollision(m_player->GetCollider().GetColliderBox(), m_bombs[i]->GetCollider().GetColliderBox()))
 		{
-			m_bombs[i]->Render();
+			m_player->SetHealth(m_bombs[i]->GetDamage());
+			delete m_bombs[i];
+			m_bombs.erase(m_bombs.begin() + i);
 		}
+
 	}
+
 }
 
 void PlayState::SpawnBombs(const unsigned amount, int startingPosition)
@@ -96,7 +109,7 @@ void PlayState::SpawnBombs(const unsigned amount, int startingPosition)
 	}
 
 	std::mt19937 rng(std::random_device{}());
-	std::uniform_int_distribution<int> distributionXPosition(startingPosition, 30 * 32);
+	std::uniform_int_distribution<int> distributionXPosition(startingPosition, 45 * 32);
 
 	for (unsigned i = 0; i < amount; i++)
 	{
--- The platformer/PlayState.h
+++ The platformer/PlayState.h
@@ -5,6 +5,7 @@
 #include "Engine/Map.h"
 #include "Player.h"
 #include "Bombs.h"
+#include "Goal.h"
 
 class PlayState : public GameState
 {
@@ -16,11 +17,15 @@ public:
 	virtual void Update(const float dt);
 	virtual void Render();
 private:
+	void BombCollision(const float dt);
 	void SpawnBombs(const unsigned amount, int startingPosition = 250);
 private:
+	std::string m_level;
+	std::string m_levelPath;
 	int m_currentMap = 0;
 	std::vector<Map*> m_maps;
 	std::vector<Bombs*> m_bombs;
 	Player* m_player = nullptr;
+	Goal* m_goal = nullptr;
 };
 
