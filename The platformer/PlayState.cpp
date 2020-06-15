fade3b99d15532d138fcfbc2d51413fdb4b9197d
Nameless <tyrebrown873@gmail.com>
2020-06-15 09:19:19 -0400

    ints

--- The platformer/PlayState.cpp
+++ The platformer/PlayState.cpp
@@ -4,15 +4,20 @@
 #include "Engine/TextureManager.h"
 #include "Engine/Camera.h"
 #include "Engine/InputHandler.h"
+#include "Engine/Music.h"
 
 PlayState::PlayState()
 {
 	m_maps.emplace_back(new Map());
+	Music::GetInstance()->LoadMusic("fath", "Sounds/Faith by Richard Burnish.ogg");
+	Music::GetInstance()->Play("fath");
+	Music::GetInstance()->GetMusic("fath").setLoop(true);
+	TextureManager::GetInstance()->LoadFont("font", "Font/times.ttf");
 	MapParser::GetInstance()->Load("level1", "Levels/level1.tmx");
 	m_maps[m_currentMap] = MapParser::GetInstance()->GetMap("level1");
 	CollisionHandler::GetInstance()->Init(m_maps[m_currentMap]);
 	TextureManager::GetInstance()->LoadTexture("player", "Textures/player.png");
-	m_player = new Player(GameObjectProperties("player", {32.f, 0.f}, 46, 50));
+	m_player = new Player(GameObjectProperties("player", {32, 0}, 46, 50));
 	Camera::GetInstance()->SetTarget(m_player->GetOrigin());
 }
 
@@ -25,14 +30,6 @@ PlayState::~PlayState()
 void PlayState::HandleInput()
 {
 	Camera::GetInstance()->SetTarget(m_player->GetOrigin());
-	if (InputHandler::GetInstance()->GetKeyDown(sf::Keyboard::Key::C))
-	{
-		m_maps.emplace_back(new Map());
-		MapParser::GetInstance()->Load("level2", "Levels/level2.tmx");
-		m_currentMap++;
-		m_maps[m_currentMap] = MapParser::GetInstance()->GetMap("level2");
-		CollisionHandler::GetInstance()->Init(m_maps[m_currentMap]);
-	}
 }
 
 void PlayState::Update(const float dt)
@@ -44,6 +41,7 @@ void PlayState::Update(const float dt)
 void PlayState::Render()
 {
 	TextureManager::GetInstance()->Draw("mainMenuBackground", 0, 0);
+	TextureManager::GetInstance()->DrawText("font", "Press C to go to the next level!", 50, 50, 50);
 	m_maps[m_currentMap]->Render();
 	m_player->Render();
 }
